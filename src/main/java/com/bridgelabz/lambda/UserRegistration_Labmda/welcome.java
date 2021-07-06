4ea18fb719a9f7b9f7a358ec22153ed20c1a3cf1
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-07-06 13:10:18 +0530

    Added : Welcome To User Registration Lambda Program

---
+++ src/main/java/com/bridgelabz/lambda/UserRegistration_Labmda/welcome.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.lambda.UserRegistration_Labmda;
+
+public class welcome {
+	public static void main(String[] args) {
+		System.out.println("Welcome To User Registration Lamda Program");
+	}
+}
